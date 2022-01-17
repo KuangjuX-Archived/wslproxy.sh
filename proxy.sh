aa56841f5bf50455d4e479a998ffd0b9d99bc51c
KuangjuX <18630816527@163.com>
2022-01-17 20:33:10 +0800

    update

--- proxy.sh
+++ proxy.sh
@@ -1,10 +1,20 @@
-HOST_IP="192.168.3.19"
 HOST_PORT="7890"
-PROXY_URL="http://$HOST_IP:$HOST_PORT"
-export ALL_PROXY=$PROXY_URL
-export HTTP_PROXY=$PROXY_URL
-export HTTPS_PROXY=$PROXY_URL
+GIT_CONFIG_FILE="/home/kuangjux/.ssh/config"
+
+# 用户输入宿主机 ip
+echo -n "请输入宿主机IP:"
+read HOST_IP
+PROXY_HTTP="http://$HOST_IP:$HOST_PORT"
+PROXY_HTTPS="https://$HOST_IP:$HOST_PORT"
+
+# export 代理
+export ALL_PROXY=$PROXY_HTTP
+
+# 配置 git
+git config --global http.proxy $PROXY_HTTP
+git config --global https.proxt $PROXY_HTTPS
+echo "Host github.com\n" > $GIT_CONFIG_FILE
+echo "    User git" >> $GIT_CONFIG_FILE
+echo "    ProxyCommand nc -v -x $HOST_IP:$HOST_PORT %h %p" >> $GIT_CONFIG_FILE
 
 echo "ALL_PROXY: " $ALL_PROXY
-echo "HTTP_PROXY: " $HTTP_PROXY
-echo "HTTPS_PROXY: " $HTTPS_PROXY
